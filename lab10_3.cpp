be6c20071acc8df4853114b3a288d0e37ec452c2
MeTOR1657 <chokun246810@gmail.com>
2024-12-20 18:18:44 +0700

    Completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,44 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int count = 0;
+    float sum = 0.0;
+    float sum_of_square = 0.0;
+    string textline;
+
+    ifstream source("score.txt"); 
+
+    if (!source.is_open()) {
+        cerr << "Error: Unable to open file." << endl;
+        return 1;
+    }
+
+    while (getline(source, textline)) {
+        float value = stof(textline); 
+        sum += value;                
+        sum_of_square += value * value; 
+        count++;                     
+    }
+
+    source.close();
+
+
+    float mean = sum / count;
+    float stddev = sqrt((sum_of_square / count) - (mean * mean));
+
+
+    cout << "Number of data = " << count << "\n";
+
+
+    cout << defaultfloat << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << stddev << "\n";
+
+    return 0;
+}
